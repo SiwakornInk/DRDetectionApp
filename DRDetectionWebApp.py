6448ca1bd323c45018cf279b0b57f0478620f763
SiwakornInk <107915130+SiwakornInk@users.noreply.github.com>
2023-08-24 21:18:47 +0700

    Update DRDetectionWebApp.py

--- DRDetectionWebApp.py
+++ DRDetectionWebApp.py
@@ -1,114 +1,139 @@
+#import library
 import cv2
 import time
+import scipy 
+
 import numpy as np
 import streamlit as st
+import random as python_random
+
 from PIL import Image
 from tensorflow import keras
 from keras.preprocessing.image import ImageDataGenerator
 
-# Constants
+################################# SETUP #################################
+
+model = keras.models.load_model('Prepro94.75')
+
 WIDE = 128
 BATCH_SIZE = 1
 
-# Load the trained model
-model = keras.models.load_model('Prepro94.75')
-
-# Image preprocessing
-def load_ben_color(img, gaussian_weight):
-    image = cv2.addWeighted(img, 2, cv2.GaussianBlur(img, (0, 0), WIDE / gaussian_weight), -2, 100)
+def load_ben_color(img):
+    image = cv2.addWeighted(img,2, cv2.GaussianBlur( img , (0,0) , WIDE/5) ,-2 ,100)
     return image
 
-def crop_image_from_gray(img, crop_tolerance):
-    if img.ndim == 2:
-        mask = img > crop_tolerance
-        return img[np.ix_(mask.any(1), mask.any(0))]
-    elif img.ndim == 3:
+def crop_image_from_gray(img,tol=7):
+    if img.ndim ==2:
+        mask = img>tol
+        return img[np.ix_(mask.any(1),mask.any(0))]
+    elif img.ndim==3:
         gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        mask = gray_img > crop_tolerance
+        mask = gray_img>tol
         
-        check_shape = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))].shape[0]
-        if check_shape == 0:
-            return img
+        check_shape = img[:,:,0][np.ix_(mask.any(1),mask.any(0))].shape[0]
+        if (check_shape == 0): # image is too dark so that we crop out everything,
+            return img # return original image
         else:
-            img1 = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))]
-            img2 = img[:, :, 1][np.ix_(mask.any(1), mask.any(0))]
-            img3 = img[:, :, 2][np.ix_(mask.any(1), mask.any(0))]
-            img = np.stack([img1, img2, img3], axis=-1)
+            img1=img[:,:,0][np.ix_(mask.any(1),mask.any(0))]
+            img2=img[:,:,1][np.ix_(mask.any(1),mask.any(0))]
+            img3=img[:,:,2][np.ix_(mask.any(1),mask.any(0))]
+    #         print(img1.shape,img2.shape,img3.shape)
+            img = np.stack([img1,img2,img3],axis=-1)
+    #         print(img.shape)
         return img
-
-def circle_crop(img, crop_tolerance, gaussian_weight):
-    img = crop_image_from_gray(img, crop_tolerance)
+    
+def circle_crop(img):   
+    """
+    Create circular crop around image centre    
+    """    
+    img = crop_image_from_gray(img)    
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
-    height, width, depth = img.shape
-    x = int(width / 2)
-    y = int(height / 2)
-    r = np.amin((x, y))
+    height, width, depth = img.shape    
+    
+    x = int(width/2)
+    y = int(height/2)
+    r = np.amin((x,y))
     
     circle_img = np.zeros((height, width), np.uint8)
-    cv2.circle(circle_img, (x, y), int(r), 1, thickness=-1)
+    cv2.circle(circle_img, (x,y), int(r), 1, thickness=-1)
     img = cv2.bitwise_and(img, img, mask=circle_img)
-    img = crop_image_from_gray(img, crop_tolerance)
-    img = load_ben_color(img, gaussian_weight)
-    return img
+    img = crop_image_from_gray(img)
+    return img 
 
-# Streamlit app
+################################# Main #################################
+
+#Title
 st.title('👁️ Diabetic Retinopathy Detection')
 
-# User-friendly instructions
-st.write('Upload an eye fundus image to check for Diabetic Retinopathy.')
+#Images
+NU = Image.open('NULOGO.png')
+SCI = Image.open('SciLOGO.webp')
+IMAGES = [NU,SCI]
+DRex = Image.open('DRLabel.jpg')
+DRprepro = Image.open('PreproEX.png')
 
-# Image preprocessing settings
-gaussian_weight = st.slider('Gaussian Blur Weight', 0, 10, 2)
-crop_tolerance = st.slider('Cropping Tolerance', 0, 50, 7)
+#Sidebar
+st.sidebar.title('Contact me :')
+st.sidebar.markdown('Siwakorn Inkong-ngam')
+st.sidebar.markdown('Tel : (+66)9 3308 5230')
+st.sidebar.markdown('Email : Siwakorni62@nu.ac.th')
+st.sidebar.image(IMAGES,width=150)
 
-# Input image and Predict
-input_image = st.file_uploader("Upload Image (.jpg, .jpeg or .png)", type=['jpg', 'jpeg', 'png'], accept_multiple_files=False)
+#Markdown
+st.markdown("""
+            This app provides an image classification to detect the Diabetic Retinopathy in a fundus image using our trained CNN model
+            and a dataset of fundus images from DeepDRiD website. \n 
+            * **Python libraries :** streamlit, cv2, pillow, numpy, pandas, keras, os
+            * **Data source :** DeepDriD website, https://isbi.deepdr.org/data.html
+            """)
 
-if input_image:
-    # Preprocess the uploaded image based on the settings
-    st.image(input_image, caption='Uploaded Fundus Image', width=256)
+st.markdown("""
+            Diabetic Retinopathy is a complication of diabetes, caused by high blood sugar levels damaging the back of the eye (retina). 
+            It can cause blindness if left undiagnosed and untreated. However, it usually takes several years for Diabetic Retinopathy 
+            to reach a stage where it could threaten your sight.
+            """)
+
+st.image(DRex,width=700,caption='Simple picture to explain Diabetic Retinopathy')
+
+st.markdown("""
+            Before checking Diabetic Retinopathy from a fundus image, we first use our preprocessing method to 
+            detect some signs of them easier. This is a demonstration from our preprocessing method : 
+            """)
+
+st.image(DRprepro,width=700,caption='Example of our preprocessing')
+
+st.markdown("""
+            Then we use trained CNN model to detect some signs of Diabetic Retinopathy and show you the result.
+            """) 
+
+#Input image and Predict
+input_image = st.file_uploader("Please upload your fundus image (.jpg, .jpeg or .png) : ", type=['jpg', 'jpeg', 'png'], accept_multiple_files=False)
+
+if input_image :
+    st.image(input_image, caption = 'This is your fundus image.', width=256)
     image_str = input_image.read()
-    nparr = np.frombuffer(image_str, np.uint8)
+    nparr = np.fromstring(image_str, np.uint8)
     img_bgr = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    img_rgb = circle_crop(img_bgr, crop_tolerance, gaussian_weight)
-    img_rgb_resized = cv2.resize(img_rgb, (WIDE, WIDE))
-    img_rgb_resized = img_rgb_resized.reshape(BATCH_SIZE, WIDE, WIDE, 3)
+    img_rgb = circle_crop(img_bgr)
+    img_rgb_resized = cv2.resize(img_rgb,(WIDE,WIDE))
+    img_rgb_resized  = img_rgb_resized.reshape(BATCH_SIZE,WIDE,WIDE,3)
 
-    datagen = ImageDataGenerator(rescale=1. / 255)
+    datagen = ImageDataGenerator(rescale= 1./255, preprocessing_function=load_ben_color)
     testdata = datagen.flow(img_rgb_resized)
-
-    if st.button('Check for Diabetic Retinopathy'):
+    
+    if st.button('Click for checking the Diabetic Retinopathy'):
         with st.spinner('Predicting...'):
             time.sleep(2)
         predict = model.predict(testdata)
-        probability_dr = predict[0][1]
-        probability_no_dr = predict[0][0]
-
         Classes = np.argmax(predict)
-        if Classes == 0:
-            st.success(f"""
-                **Prediction Result:** This image has no signs of Diabetic Retinopathy. You are healthy. 
-                \nProbability of having DR: {probability_dr:.2f}
-                \nProbability of not having DR: {probability_no_dr:.2f}
-            """)
-        else:
-            st.warning(f"""
-                **Prediction Result:** This image is suspected to have Diabetic Retinopathy. We recommend consulting a specialist for further evaluation.
-                \nProbability of having DR: {probability_dr:.2f}
-                \nProbability of not having DR: {probability_no_dr:.2f}
-            """)
-
-# Image Preprocessing Explanation
-st.subheader('Image Preprocessing Explanation')
-st.write("""
-- **Gaussian Blur Weight:** Adjust this value to control the amount of Gaussian blur applied to the image.
-- **Cropping Tolerance:** Tune this value to determine how much of the image is cropped during preprocessing.
-""")
-
-# Feedback and Credits
-st.subheader('Feedback & Credits')
-st.write("""
-- If you encounter issues or have suggestions for improvement, please provide feedback.
-- App developed by [Your Name].
-""")
+        if Classes == 0 : 
+            st.write("""
+                        Prediction for this image : \n
+                        This image has no DR sign. You are healthy. 
+                    """)
+        else :
+            st.write("""
+                        Prediction for this image : \n
+                        This image is suspected of Diabetic Retinopathy. We recommend you make an appointment with a specialized doctor for more investigation.
+                    """) 
