050f88cca6d9c21445d08edb48d6ddc5cac54a4e
SiwakornInk <107915130+SiwakornInk@users.noreply.github.com>
2023-08-24 21:08:13 +0700

    Update DRDetectionWebApp.py

--- DRDetectionWebApp.py
+++ DRDetectionWebApp.py
@@ -13,8 +13,43 @@ BATCH_SIZE = 1
 # Load the trained model
 model = keras.models.load_model('Prepro94.75')
 
-# Functions for image preprocessing
-# ...
+# Image preprocessing
+def load_ben_color(img):
+    image = cv2.addWeighted(img, 2, cv2.GaussianBlur(img, (0, 0), WIDE / 5), -2, 100)
+    return image
+
+def crop_image_from_gray(img, tol=7):
+    if img.ndim == 2:
+        mask = img > tol
+        return img[np.ix_(mask.any(1), mask.any(0))]
+    elif img.ndim == 3:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        mask = gray_img > tol
+        
+        check_shape = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))].shape[0]
+        if check_shape == 0:
+            return img
+        else:
+            img1 = img[:, :, 0][np.ix_(mask.any(1), mask.any(0))]
+            img2 = img[:, :, 1][np.ix_(mask.any(1), mask.any(0))]
+            img3 = img[:, :, 2][np.ix_(mask.any(1), mask.any(0))]
+            img = np.stack([img1, img2, img3], axis=-1)
+        return img
+
+def circle_crop(img):
+    img = crop_image_from_gray(img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    height, width, depth = img.shape
+    x = int(width / 2)
+    y = int(height / 2)
+    r = np.amin((x, y))
+    
+    circle_img = np.zeros((height, width), np.uint8)
+    cv2.circle(circle_img, (x, y), int(r), 1, thickness=-1)
+    img = cv2.bitwise_and(img, img, mask=circle_img)
+    img = crop_image_from_gray(img)
+    return img
 
 # Streamlit app
 st.title('👁️ Diabetic Retinopathy Detection')
